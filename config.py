2d5d2ec0a2dcb8c30bcb0f84a2893b1d929ec081
Toxic <gyaneshp606@gmail.com>
2026-03-22 23:55:50 +0530

    Iam-Toxicc

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ API_ID = 123456
 API_HASH = "api_hash"
 BOT_TOKEN = "bot_token"
 
-MONGO_URL = "mongodb_url"
+MONGO_URL = "mongodb+srv://Toxicbots:TOXIC0000@cluster0.u5sllsx.mongodb.net/?appName=Cluster0"
 BOT_USERNAME = "ZyroqrGenbot"
