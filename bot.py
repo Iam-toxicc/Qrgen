218e8c58b2981f07d20502cf948c6974476fb2d2
Toxic <gyaneshp606@gmail.com>
2026-03-23 00:20:06 +0530

    I

--- bot.py
+++ bot.py
@@ -2,18 +2,11 @@
 # Copyright (c) 2026 Gyanesh Patel
 # All rights reserved.
 
-from pyrogram import Client
-import config
+from app import app
 
 print("🔥 Bot Starting...")
 
-app = Client(
-    "Toxicqrbot",
-    api_id=config.API_ID,
-    api_hash=config.API_HASH,
-    bot_token=config.BOT_TOKEN
-)
-
+# IMPORT MODULE AFTER app
 import modules.qr
 
 print("✅ Modules Loaded")
